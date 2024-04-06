6aa9d1f4bbbad520c9247a38ad9ce91ecab7625d
Diyora <yukine3012@gmail.com>
2024-04-06 21:38:49 +0500

    new

--- src/App.js
+++ src/App.js
@@ -1,35 +1,17 @@
-import { Fragment } from "react";
-import SelectVariants from "./pages/select";
 import './App.css';
-import Logo from './img/Logo.png'
-// import { Link, Route, Routes } from "react-router-dom";
 import * as React from 'react';
-import TextField from '@mui/material/TextField';
-import Stack from '@mui/material/Stack';
-import Button from '@mui/material/Button';
+import Header from './components/header';
+import Footer from './components/footer';
 
 function App() {
   return (
-    <Fragment>
-      <header className="header">
-        <div className="navbar">
-          <img className="navbar__logo" src={Logo} alt="logo" />
+    <div className='wrapper'>
+      <Header/>
 
-          <TextField id="outlined-basic" label="Search bar" variant="outlined" className="navbar__search" />
+       
 
-          <SelectVariants />
-
-          {/* <Link to={"/"} className="header__menu-link">Become a Mentor</Link> */}
-
-          <Stack spacing={2} direction="row" className="buttons">
-            <Button variant="outlined">Login</Button>
-            <Button variant="contained">Sing Up</Button>
-          </Stack>
-
-        </div>
-
-      </header>
-    </Fragment>
+      <Footer/>
+    </div>
   );
 }
 
---
+++ src/components/header.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import Logo from '../img/Logo.png'
+
+export default function Header() {
+    return (
+        <header>
+            <div className='navbar'>
+
+                <img src={Logo} alt="logo" className='logo' />
+                <input type="search" placeholder='Search bar' className='searchbar' />
+
+                <div className='options'>
+                    <label htmlFor='#select'>Category</label>
+
+                    <select id='select'>
+                        <option>
+
+                        </option>
+                        <option>
+
+                        </option>
+                        <option>
+
+                        </option>
+                    </select>
+                </div>
+
+                <a href="!" className='mentor'>Become a Mentor</a>
+
+                <div className='buttons'>
+                    <button className='login'>Login</button>
+                    <button className='singup'>Sing Up</button>
+                </div>
+
+
+
+            </div>
+        </header>
+    )
+}
