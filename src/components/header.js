9d1dfbbe22a12bfac8ad230ffda760667fbf7870
Diyora <yukine3012@gmail.com>
2024-04-07 12:38:14 +0500

    new

--- src/components/header.js
+++ src/components/header.js
@@ -1,61 +1,64 @@
 import React from 'react'
 import Logo from '../img/Logo.png'
+import time from '../img/icons/time.svg'
+import mentor from '../img/icons/mentor.svg'
+import video from '../img/icons/video.svg'
 
 export default function Header() {
     return (
         <header>
-            <div className='fix'>
+            
                 <div className='navbar'>
 
-                <img src={Logo} alt="logo" className='logo' />
-                <input type="search" placeholder='Search bar' className='searchbar' />
+                    <img src={Logo} alt="logo" className='logo' />
+                    <input type="search" placeholder='Search bar' className='searchbar' />
 
-                <div className='options'>
-                    <label htmlFor='#select'>Category</label>
+                    <div className='options'>
+                        <label htmlFor='#select'>Category</label>
 
-                    <select id='select'>
-                        <option>
+                        <select id='select'>
+                            <option>
 
-                        </option>
-                        <option>
+                            </option>
+                            <option>
 
-                        </option>
-                        <option>
+                            </option>
+                            <option>
 
-                        </option>
-                    </select>
+                            </option>
+                        </select>
+                    </div>
+
+                    <a href="!" className='mentor'>Become a Mentor</a>
+
+                    <div className='buttons'>
+                        <button className='login'>Login</button>
+                        <button className='singup'>Sing Up</button>
+                    </div>
+
                 </div>
-
-                <a href="!" className='mentor'>Become a Mentor</a>
-
-                <div className='buttons'>
-                    <button className='login'>Login</button>
-                    <button className='singup'>Sing Up</button>
-                </div>
-
-            </div>
-            </div>
+           
 
             <div className='info'>
                 <h1 className='info__title'>Find the Best Courses and Amazing Mentor</h1>
                 <p className='info__subtitle'>Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor rhoncus </p>
-                <ul features>
-                    <li className=''>
-                    <i class="bi bi-stopwatch"></i>
+                <ul>
+                    <li className='feature'>
+                        <img src={time} alt="icon" />
                         <p>
-Life time acces
+                            Life Time Acces
                         </p>
                     </li>
-                    <li className=''>
-                        <img src="" alt="" />
+                    <li className='feature'>
+                        <img src={mentor} alt="icon" />
                         <p>
-
+                            Expert Mentor
                         </p>
                     </li>
-                    <li className=''>
-                        <img src="" alt="" />
+                    <li className='feature'>
+                        <img src={video} alt="icon" />
                         <p>
-
+                            100K+ Courses
                         </p>
                     </li>
                 </ul>
