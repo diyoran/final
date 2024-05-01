df941b7b686c4b0bf9405f1af10c244c38722309
Diyora <yukine3012@gmail.com>
2024-05-01 23:54:27 +0500

    accordion added cousePage almost done

---
+++ src/components/Accordion/accordion.js
@@ -0,0 +1,35 @@
+import Accordion from 'react-bootstrap/Accordion';
+import '../Accordion/accordion.scss'
+
+function AllCollapseExample() {
+  return (
+    <Accordion  className="accordion">
+      <Accordion.Item className="accordion" eventKey="0">
+        <Accordion.Header className="accordion__title">Accordion Item #1</Accordion.Header>
+        <Accordion.Body className="accordion__txt">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item  className="accordion" eventKey="1">
+        <Accordion.Header className="accordion__title">Accordion Item #2</Accordion.Header>
+        <Accordion.Body className="accordion__txt">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </Accordion.Body>
+      </Accordion.Item>
+    </Accordion>
+  );
+}
+
+export default AllCollapseExample;
--- src/pages/coursePage.js
+++ src/pages/coursePage.js
@@ -1,5 +1,9 @@
 import React from "react";
 import { Fragment } from "react";
+import share from "../img/icons/share.svg"
+import plus from "../img/icons/plus-lg.svg"
+import mentorPhoto from "../img/tutorPhoto.jpg"
+import AllCollapseExample from "../components/Accordion/accordion";
 
 export default function CoursePage() {
     return (
@@ -7,38 +11,141 @@ export default function CoursePage() {
             <div className="page">
                 <div className="page__header">
 
-                    <h2 className="page__title">
-                        How to become an Expert Web Developer
-                    </h2>
-                    <p>
-                        rating ⭐⭐⭐⭐⭐
-                    </p>
 
-                    <div className="page__tutor">
-                        <div className="page__tutor--photo">
+                    <div className="page__header-content">
+                        <h2 className="page__title">
+                            How to become an Expert Web Developer
+                        </h2>
 
+                        <h4 className="page__subtitle">
+                            By Sebastian Michaelis
+                        </h4>
+
+
+                        <div className="page__header__btn">
+                            <button className="page__add">
+                                <p>
+                                    Add To Wishlist
+                                </p>
+                                <img src={plus} alt="error" />
+                            </button>
+
+                            <button className="page__share">
+                                <p>
+                                    Share
+                                </p>
+                                <img src={share} alt="error" />
+                            </button>
                         </div>
-                        <p className="page__tutor--name">
-                            Pak Arif Satria Kecek ulalala
-                        </p>
                     </div>
 
-                    <button className="page__btn--add">
-                        <p>
-                            Add To Wishlist
-                        </p>
-                        <img src="" alt="error" />
-                    </button>
+                </div>
 
-                    <button className="page__btn--share">
-                        <p>
-                            Share
-                        </p>
-                        <img src="" alt="error" />
-                    </button>
-
+                <div className="tutor">
+                    <h2>
+                        Mentor
+                    </h2>
+                    <div className="tutor__info">
+                        <img className="tutor__info-img" src={mentorPhoto} alt="error" />
+                        <div className="tutor__info-text">
+                            <h3>
+                                Sebastian Michaelis
+                            </h3>
+                            <p>
+                                Senior developer expert instructor
+                            </p>
+                            {/* rating */}
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Feugiat eu in orci, nunc amet libero. Nam scelerisque vestibulum bibendum a turpis.
+                            </p>
+                            {/* counter of courses */}
+                        </div>
+                    </div>
 
                 </div>
+
+                <div className="course">
+                    <div className="course__info">
+                        <h2>
+                            What you’ll learn from this course
+                        </h2>
+
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Feugiat eu in orci, nunc amet libero. Nam scelerisque vestibulum bibendum a turpis. Ante feugiat lectus massa, odio amet. Auctor sit mattis non id proin elit placerat. Lectus morbi amet et aliquam magna mauris. Proin pulvinar fringilla nunc, tristique urna, massa, tincidunt.</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Feugiat eu in orci, nunc amet libero. Nam scelerisque vestibulum bibendum a turpis. Ante feugiat lectus massa, odio amet. Auctor sit mattis non id proin elit placerat. Lectus morbi amet et aliquam magna mauris. Proin pulvinar fringilla nunc, tristique urna, massa, tincidunt.</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Feugiat eu in orci, nunc amet libero. Nam scelerisque vestibulum bibendum a turpis. Ante feugiat lectus massa, odio amet. Auctor sit mattis non id proin elit placerat. Lectus morbi amet et aliquam magna mauris. Proin pulvinar fringilla nunc, tristique urna, massa, tincidunt.</p>
+
+                    </div>
+
+                    <div className="course__features">
+                        <h2>
+                            Objective Features
+                        </h2>
+                        <ul>
+                            <li>
+                                Build fully websites and web apps for your startup or business.
+                            </li>
+                            <li>
+                                Build fully websites and web apps for your startup or business.
+                            </li>
+                            <li>
+                                Build fully websites and web apps for your startup or business.
+                            </li>
+                            <li>
+                                Build fully websites and web apps for your startup or business.
+                            </li>
+                            <li>
+                                Build fully websites and web apps for your startup or business.
+                            </li>
+                            <li>
+                                Build fully websites and web apps for your startup or business.
+                            </li>
+                            <li>
+                                Build fully websites and web apps for your startup or business.
+                            </li>
+                            <li>
+                                Build fully websites and web apps for your startup or business.
+                            </li>
+                            <li>
+                                Build fully websites and web apps for your startup or business.
+                            </li>
+                            <li>
+                                Build fully websites and web apps for your startup or business.
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="course__requirement">
+                        <h2>
+                            Requirement
+                        </h2>
+                        <ul>
+                            <li>
+                                No programming experience needed - I'll teach you everything you need to know
+                            </li>
+                            <li>
+                                A Mac or PC computer with access to the internet
+                            </li>
+                            <li>
+                                No previous coding experience is needed
+                            </li>
+                            <li>
+                                All tools and software used in this course will be free
+                            </li>
+                            <li>
+                                Any operating system: Mac, PC, Linux
+                            </li>
+
+                        </ul>
+                    </div>
+
+                    <div className="course__accordion">
+                        <h2>
+                            Module Courses
+                        </h2>
+                        <AllCollapseExample />
+                    </div>
+
+                </div>
+
             </div>
         </Fragment>
     )
